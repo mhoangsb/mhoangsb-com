b4f2c44042da92f79fa2c472db4f7d27085fc57b
mhoang <mhoangsb@gmail.com>
2025-04-24 23:17:55 +0700

    fix: change <div> to <button> for accessibility in <LanguageSelector />

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -11,21 +11,21 @@ export default function LanguageSelector() {
 
   return (
     <div className="absolute right-0 top-[50%] flex w-14 -translate-y-[50%] items-center justify-between text-sm text-gray-500 sm:text-base">
-      <div
+      <button
         className={`cursor-pointer hover:text-white ${currentLanguage === "vi" ? "text-gray-300" : ""}`}
         onClick={() => setCurrentLanguage("vi")}
       >
         vi
-      </div>
+      </button>
 
       <div className="h-4 w-0.5 bg-gray-400"></div>
 
-      <div
+      <button
         className={`cursor-pointer hover:text-white ${currentLanguage === "en" ? "text-gray-300" : ""}`}
         onClick={() => setCurrentLanguage("en")}
       >
         en
-      </div>
+      </button>
     </div>
   );
 }
